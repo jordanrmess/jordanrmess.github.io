c0f732e984ac7ebd5a7450133a3aac0e3804e50f
jordanrmess <jordanrmess@gmail.com>
2019-09-20 22:08:04 -0400

    fixing some bugs

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+ScrollReveal({ duration: 1000 ,reset:true})
+ScrollReveal().reveal('#about_header')
+ScrollReveal().reveal('#row1', { delay: 500 })
+ScrollReveal().reveal('#row2', { delay: 1000 })
+ScrollReveal().reveal('#row3', { delay: 1500 })
+
+
+ScrollReveal().reveal('#contact_header')
+ScrollReveal().reveal('#icons', { delay: 500 })
