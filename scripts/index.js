9a54871e911080ab2f12481fc4eba9a21e59a0d8
jordanrmess <jordanrmess@gmail.com>
2019-12-19 15:48:15 -0500

    updating contact page

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
 ScrollReveal({ duration: 1000 ,reset:true})
 ScrollReveal().reveal('#about_header')
+ScrollReveal().reveal('#about_text');
+ScrollReveal().reveal('#img');
 ScrollReveal().reveal('#row1', { delay: 500 })
 ScrollReveal().reveal('#row2', { delay: 1000 })
 ScrollReveal().reveal('#row3', { delay: 1500 })
@@ -7,3 +9,4 @@ ScrollReveal().reveal('#row3', { delay: 1500 })
 
 ScrollReveal().reveal('#contact_header')
 ScrollReveal().reveal('#icons', { delay: 500 })
+
